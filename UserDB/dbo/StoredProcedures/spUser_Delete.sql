1eadf7c4cd7f33249dc09b1282c742a772412eb4
Kirill Dyakonov <jozzzwardtm@mail.ru>
2022-03-07 22:25:50 +0300

    Добавьте файлы проекта.

---
+++ UserDB/dbo/StoredProcedures/spUser_Delete.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Delete]
+	@Id int
+AS
+begin 
+	DELETE FROM dbo.[User]
+	WHERE Id = @Id
+end
---
+++ UserDB/dbo/StoredProcedures/spUser_Get.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Get]
+	@Id int
+AS
+begin
+	SELECT FirstName, LastName FROM dbo.[User]
+	WHERE Id = @Id
+end
