fcd311e18866b1d56a5fa9c068052cf1efb4e838
Kirill Dyakonov <jozzzwardtm@mail.ru>
2022-03-08 17:20:21 +0300

    Added all(5) procedures to UserDB

---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,8 @@
+﻿if not exists (SELECT 1 FROM dbo.[User])
+begin
+	insert into dbo.[User] (FirstName, LastName)
+	VALUES ('Jim', 'Cross'),
+	('John', 'Smith'),
+	('Carla', 'Osla'),
+	('Gred', 'Gredy');
+end
